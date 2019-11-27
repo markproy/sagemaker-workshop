86bc315dc294663eb493b3f01a113d61796842ac
Mark Roy <markproy3@gmail.com>
2019-11-27 13:00:05 -0500

    clean up

---
+++ tensorflow-workshop/inference.py
@@ -0,0 +1,76 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import io
+import json
+import numpy as np
+from collections import namedtuple
+from PIL import Image
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+from numpy import argmax
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger().setLevel(logging.DEBUG)
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+HEIGHT = 224
+WIDTH  = 224
+
+Context = namedtuple('Context',
+                     'model_name, model_version, method, rest_uri, grpc_uri, '
+                     'custom_attributes, request_content_type, accept_header')
+
+def input_handler(data, context):
+
+    logging.info('in input_handler')
+    if context.request_content_type == 'application/x-image':
+
+        stream = io.BytesIO(data.read())
+        img = Image.open(stream)
+        img = img.resize((WIDTH, HEIGHT))
+        x = np.asarray(img)
+        curr_shape = x.shape
+        new_shape = (1,) + curr_shape
+        x = x.reshape(new_shape)
+        instance = preprocess_input(x)
+        final_input = instance.tolist()
+        return json.dumps({"instances": instance.tolist()})
+
+    else:
+        _return_error(415, 'Unsupported content type "{}"'.format(context.request_content_type or 'Unknown'))
+
+    return data
+
+def output_handler(data, context):
+    """Post-process TensorFlow Serving output before it is returned to the client.
+
+    Args:
+        data (obj): the TensorFlow serving response
+        context (Context): an object containing request and configuration details
+
+    Returns:
+        (bytes, string): data to return to client, response content type
+    """
+    if data.status_code != 200:
+        raise Exception(data.content.decode('utf-8'))
+    response_content_type = context.accept_header
+    prediction = data.content
+    return prediction, response_content_type
+
+
+def _return_error(code, message):
+    raise ValueError('Error: {}, {}'.format(str(code), message))
