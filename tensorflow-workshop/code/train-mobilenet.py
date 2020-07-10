b3d34887e09ea98c23ccd16dcd520a98cf755db2
Mark Roy <markproy3@gmail.com>
2020-07-10 10:09:44 -0400

    add spot training

--- tensorflow-workshop/code/train-mobilenet.py
+++ tensorflow-workshop/code/train-mobilenet.py
@@ -14,28 +14,72 @@
 import tensorflow as tf
 import tensorflow.keras
 
+TF_VERSION = tf.version.VERSION
+print('TF version: {}'.format(tf.__version__))
+print('Keras version: {}'.format(tensorflow.keras.__version__))
+
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
 LAST_FROZEN_LAYER = 20
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout, GlobalAveragePooling2D
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.optimizers import SGD, Adam, RMSprop
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras import backend as K
 
-TF_VERSION = tf.version.VERSION
-print('TF version: {}'.format(tf.__version__))
-print('Keras version: {}'.format(tensorflow.keras.__version__))
-
 import numpy as np
 import os
+import re
 import json
 import argparse
 import glob
+import boto3
 
 HEIGHT = 224
 WIDTH  = 224
+
+class PurgeCheckpointsCallback(tensorflow.keras.callbacks.Callback):
+    def __init__(self, args):
+        """ Save params in constructor
+        """
+        self.args = args
+        
+    def on_epoch_end(self, epoch, logs=None):
+        files = sorted([f for f in os.listdir(self.args.checkpoint_path) if f.endswith('.' + 'h5')])
+        keep  = 3
+        print(f'    End of epoch {epoch + 1}. Removing old checkpoints...')
+        for i in range(0, len(files) - keep - 1):
+            print(f'    local: {files[i]}')
+            os.remove(f'{self.args.checkpoint_path}/{files[i]}')
+            
+            s3_uri_parts = self.args.s3_checkpoint_path.split('/')
+            bucket = s3_uri_parts[2]
+            key = '/'.join(s3_uri_parts[3:]) + '/' + files[i]
+
+            print(f'    s3 bucket: {bucket}, key: {key}')
+            s3_client = boto3.client('s3')
+            s3_client.delete_object(Bucket=bucket, Key=key)
+        print(f'      Done\n')
+
+def load_checkpoint_model(checkpoint_path):
+    files = sorted([f for f in os.listdir(checkpoint_path) if f.endswith('.' + 'h5')])
+    epoch_numbers = [re.search('(?<=\.)(.*[0-9])(?=\.)',f).group() for f in files]
+      
+    max_epoch_number = max(epoch_numbers)
+    max_epoch_index = epoch_numbers.index(max_epoch_number)
+    max_epoch_filename = files[max_epoch_index]
+    
+    print('\nList of available checkpoints:')
+    print('------------------------------------')
+    [print(f) for f in files]
+    print('------------------------------------')
+    print(f'Checkpoint file for latest epoch: {max_epoch_filename}')
+    print(f'Resuming training from epoch: {max_epoch_number}')
+    print('------------------------------------')
+    
+    resume_model = load_model(f'{checkpoint_path}/{max_epoch_filename}')
+    return resume_model, int(max_epoch_number)
 
 def build_finetune_model(base_model, dropout, fc_layers, num_classes):
     # Freeze all base layers
@@ -55,7 +99,6 @@ def build_finetune_model(base_model, dropout, fc_layers, num_classes):
     finetune_model = Model(inputs=base_model.input, outputs=predictions)
 
     return finetune_model
-
 
 def create_data_generators(args):
     train_datagen =  ImageDataGenerator(
@@ -135,7 +178,6 @@ def main(args):
     print('\nBeginning training...')
     
     NUM_EPOCHS  = args.fine_tuning_epochs
-    FINE_TUNING = True
     
     num_train_images = len(train_gen.filepaths)
     num_val_images   = len(val_gen.filepaths)
@@ -146,12 +188,12 @@ def main(args):
 
     print('Batch size: {}, Train Steps: {}, Val Steps: {}'.format(args.batch_size, train_steps, val_steps))
 
-    if not FINE_TUNING:
-        history = model.fit_generator(train_gen, epochs=NUM_EPOCHS, workers=8, 
-                               steps_per_epoch=train_steps, 
-                               validation_data=val_gen, validation_steps=val_steps,
-                               shuffle=True) 
-    else:
+    # If there are no checkpoints found, must be starting from scratch, so load the pretrained model
+    checkpoints_avail = os.path.isdir(args.checkpoint_path) and os.listdir(args.checkpoint_path)
+    if not checkpoints_avail:
+        if not os.path.isdir(args.checkpoint_path):
+            os.mkdir(args.checkpoint_path)
+        
         # Train for a few epochs
         model.fit_generator(train_gen, epochs=args.initial_epochs, workers=8, 
                                steps_per_epoch=train_steps, 
@@ -162,13 +204,30 @@ def main(args):
         for layer in model.layers[LAST_FROZEN_LAYER:]:
             layer.trainable = True
 
-        fine_tuning_lr = args.fine_tuning_lr
-        model.compile(optimizer=SGD(lr=fine_tuning_lr, momentum=0.9, decay=fine_tuning_lr / NUM_EPOCHS), 
-                      loss='categorical_crossentropy', metrics=['accuracy'])
-        history = model.fit_generator(train_gen, epochs=NUM_EPOCHS, workers=8, 
-                               steps_per_epoch=train_steps, 
-                               validation_data=val_gen, validation_steps=val_steps,
-                               shuffle=True)
+        initial_epoch_number = 0
+        
+    # Otherwise, start from the latest checkpoint
+    else:    
+        model, initial_epoch_number = load_checkpoint_model(args.checkpoint_path)
+
+    fine_tuning_lr = args.fine_tuning_lr
+    model.compile(optimizer=SGD(lr=fine_tuning_lr, momentum=0.9, decay=fine_tuning_lr / NUM_EPOCHS), 
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+    callbacks = []
+    if not (args.s3_checkpoint_path == ''):
+        checkpoint_names = 'img-classifier.{epoch:03d}.h5'
+        checkpoint_callback = ModelCheckpoint(filepath=f'{args.checkpoint_path}/{checkpoint_names}',
+                                              save_weights_only=False,
+                                              monitor='val_accuracy')
+        callbacks.append(checkpoint_callback)
+        callbacks.append(PurgeCheckpointsCallback(args))
+
+    history = model.fit_generator(train_gen, epochs=NUM_EPOCHS, workers=8, 
+                           steps_per_epoch=train_steps, 
+                           initial_epoch=initial_epoch_number,
+                           validation_data=val_gen, validation_steps=val_steps,
+                           shuffle=True, callbacks=callbacks)
     print('Model has been fit.')
 
     # Save the model if we are executing on the master host
@@ -177,6 +236,9 @@ def main(args):
         save_model_artifacts(model, os.environ.get('SM_MODEL_DIR'))
     else:
         print('NOT saving model, will leave that up to master host')
+
+    checkpoint_files = [f for f in os.listdir(args.checkpoint_path) if f.endswith('.' + 'h5')]  
+    print(f'\nCheckpoints: {sorted(checkpoint_files)}')
 
     print('\nExiting training script.\n')
     
@@ -190,6 +252,7 @@ if __name__=='__main__':
     parser.add_argument('--fine_tuning_lr', type=float, default=0.00001)
     parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--num_fully_connected_layers', type=int, default=1)
+    parser.add_argument('--s3_checkpoint_path', type=str, default='')
     # input data and model directories
     parser.add_argument('--model_dir', type=str)
     parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
@@ -197,6 +260,7 @@ if __name__=='__main__':
     parser.add_argument('--validation', type=str, default=os.environ.get('SM_CHANNEL_VALIDATION'))
     parser.add_argument('--current-host', type=str, default=os.environ.get('SM_CURRENT_HOST'))
     parser.add_argument('--hosts', type=list, default=json.loads(os.environ.get('SM_HOSTS')))
+    parser.add_argument('--checkpoint_path', type=str, default='/opt/ml/checkpoints')
 
     args, _ = parser.parse_known_args()
     print('args: {}'.format(args))
